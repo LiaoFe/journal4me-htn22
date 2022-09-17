a853c99acbe0ddb107ffbeebb3b3edf1265cf25d
Addison <74084822+addison-ch@users.noreply.github.com>
2022-09-17 18:33:17 -0400

    add mongo connection

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 app = FastAPI()
 
+password = os.environ.get('PASSWORD')
 
-@app.get("/")
+client = MongoClient(f'mongodb+srv://voicejournalhtn22:{password}@cluster0.0wr1fib.mongodb.net/?retryWrites=true&w=majority')
+db = client.test
+collection = db.testing
+
+@app.get("/upload")
 def read_root():
+    x = collection.insert_one({"Hello": "World"}).inserted_id
+
     return {"Hello": "World"}
