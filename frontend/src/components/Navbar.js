456db2ce8a0ec397593f7c223cba0bad946cfcd2
LiaoFe <felix_liao2002@hotmail.com>
2022-09-17 20:25:46 -0400

    gg

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import { Link, Switch, Route, useLocation } from 'react-router-dom';
+import {
+    Nav,
+    NavLink,
+    Bars,
+    NavMenu,
+    NavBtn,
+    NavBtnLink,
+  } from './NavbarElements';
+  
+  function NavBar2(props){    
+      return( 
+        
+        <Nav>
+
+            </Nav>
+      );
+    }
+
+  export default NavBar;
