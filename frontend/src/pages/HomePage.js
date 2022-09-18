7c991bd075699037b5b47418f6d2c0df55bca11c
LiaoFe <felix_liao2002@hotmail.com>
2022-09-17 20:32:44 -0400

    Revert "gg"
    This reverts commit 456db2ce8a0ec397593f7c223cba0bad946cfcd2.

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,138 +1,25 @@
-import MicRecorder from "mic-recorder-to-mp3"
-import { useEffect, useState, useRef } from "react"
-import axios from "axios"
+import logo from './logo.svg';
+import './App.css';
 
-  // Set AssemblyAI Axios Header
-  const assembly = axios.create({
-    baseURL: "https://api.assemblyai.com/v2",
-    headers: {
-      authorization: "ec298ba669bd4ee88ec7ad7c46a803fd",
-      "content-type": "application/json",
-      "transfer-encoding": "chunked",
-    },
-  })
-
-const App = () => {
-  // Mic-Recorder-To-MP3
-  const recorder = useRef(null) //Recorder
-  const audioPlayer = useRef(null) //Ref for the HTML Audio Tag
-  const [blobURL, setBlobUrl] = useState(null)
-  const [audioFile, setAudioFile] = useState(null)
-  const [isRecording, setIsRecording] = useState(null)
-
-  useEffect(() => {
-    //Declares the recorder object and stores it inside of ref
-    recorder.current = new MicRecorder({ bitRate: 128 })
-  }, [])
-
-  const startRecording = () => {
-    // Check if recording isn't blocked by browser
-    recorder.current.start().then(() => {
-      setIsRecording(true)
-    })
-  }
-
-  const stopRecording = () => {
-    recorder.current
-      .stop()
-      .getMp3()
-      .then(([buffer, blob]) => {
-        const file = new File(buffer, "audio.mp3", {
-          type: blob.type,
-          lastModified: Date.now(),
-        })
-        const newBlobUrl = URL.createObjectURL(blob)
-        setBlobUrl(newBlobUrl)
-        setIsRecording(false)
-        setAudioFile(file)
-      })
-      .catch((e) => console.log(e))
-  }
-
-  // AssemblyAI API
-
-  // State variables
-  const [uploadURL, setUploadURL] = useState("")
-  const [transcriptID, setTranscriptID] = useState("")
-  const [transcriptData, setTranscriptData] = useState("")
-  const [transcript, setTranscript] = useState("")
-  const [isLoading, setIsLoading] = useState(false)
-
-  // Upload the Audio File and retrieve the Upload URL
-  useEffect(() => {
-    if (audioFile) {
-      assembly
-        .post("/upload", audioFile)
-        .then((res) => setUploadURL(res.data.upload_url))
-        .catch((err) => console.error(err))
-    }
-  }, [audioFile])
-
-  // Submit the Upload URL to AssemblyAI and retrieve the Transcript ID
-  const submitTranscriptionHandler = () => {
-    assembly
-      .post("/transcript", {
-        audio_url: uploadURL,
-      })
-      .then((res) => {
-        setTranscriptID(res.data.id)
-
-        checkStatusHandler()
-      })
-      .catch((err) => console.error(err))
-  }
-
-  // Check the status of the Transcript
-  const checkStatusHandler = async () => {
-    setIsLoading(true)
-    try {
-      await assembly.get(`/transcript/${transcriptID}`).then((res) => {
-        setTranscriptData(res.data)
-      })
-    } catch (err) {
-      console.error(err)
-    }
-  }
-
-  // Periodically check the status of the Transcript
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (transcriptData.status !== "completed" && isLoading) {
-        checkStatusHandler()
-      } else {
-        setIsLoading(false)
-        setTranscript(transcriptData.text)
-
-        clearInterval(interval)
-      }
-    }, 1000)
-    return () => clearInterval(interval)
-  },)
-
-    console.log(transcriptData)
-
-
+function App() {
   return (
-    <div>
-      <h1>React Speech Recognition App</h1>
-      <audio ref={audioPlayer} src={blobURL} controls='controls' />
-      <div>
-        <button disabled={isRecording} onClick={startRecording}>
-          START
-        </button>
-        <button disabled={!isRecording} onClick={stopRecording}>
-          STOP
-        </button>
-        <button onClick={submitTranscriptionHandler}>SUBMIT</button>
-        {/* <button onClick={checkStatusHandler}>CHECK STATUS</button> */}
-      </div>
-      {transcriptData.status === "completed" ? (
-        <p>{transcript}</p>
-      ) : (
-        <p>{transcriptData.status}</p>
-      )}
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
