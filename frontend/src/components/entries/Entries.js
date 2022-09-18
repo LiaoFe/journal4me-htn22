de3ab7719dc3a52f2e757a9e67ead4c2e101ca99
Eugene Zhang <zhang.eug@gmail.com>
2022-09-17 21:13:22 -0400

    Update homepage

---
+++ frontend/src/components/entries/Entries.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./entries.css";
+
+function Entries() {
+    return (
+        <div className="container-entries">
+        </div>
+    )
+}
+
+export default Entries;
